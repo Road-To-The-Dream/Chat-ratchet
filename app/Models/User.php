ed645adcff3c573a3eac0f3907db669e646c1fbf
Sergey <fhlbc2012@gmail.com>
2019-09-02 14:54:19 +0300

    Added relations for models

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+    public function color()
+    {
+        return $this->belongsTo(Color::class)->select(['id', 'name']);
+    }
 }
